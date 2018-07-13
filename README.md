920d7f73199bdd05b1c9f6a7c5767553251352b2
nltesown <nlte@magic.fr>
2018-07-13 14:52:08 +0200

    First

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# orderly
+# Orderly
+
+« Selon une étude de l'université de Cambridge... l'odrre des ltteers dnas un mot n'a pas d'ipmrotncae. »
+
+Bon ! En attendant de remettre la main sur l'étude en question, on peut voir ce que ça donne lorsque les lettres sont rangées par ordre alphabétique (la première et dernière lettre de chaque sont conservées à leur place, c'est le principe).
---
+++ orderly.js
@@ -0,0 +1,62 @@
+$(run);
+
+function run() {
+  var anim = "wobble";
+  var $input = $("#input");
+  $(".submit").on("click", function(e) {
+    $("#logo")
+      .addClass(anim)
+      .on(
+        "animationend, oAnimationEnd, mozAnimationEnd, webkitAnimationEnd",
+        () => {
+          $input.val(orderly($input.val()));
+          $("#logo").removeClass(anim);
+        }
+      );
+  });
+}
+
+function orderly(text) {
+  return _(text)
+    .thru(d => {
+      d = " " + d.trim().replace(/\n/g, "¤") + " ";
+
+      var w = _(d)
+        .words(/[^,.;?! «»"'’\(\)¤-]+/g)
+        .value();
+      var s = _(d)
+        .words(/[,.;?! «»"'’\(\)¤-]+/g)
+        .value();
+
+      var first = s.shift();
+
+      w = _(w).map(ws =>
+        _(ws)
+          .thru(w => {
+            var first = w[0],
+              last = w.length === 1 ? "" : w[w.length - 1];
+            return (
+              first +
+              _(w)
+                .drop()
+                .dropRight()
+                .sort((a, b) => a.localeCompare(b, "fr"))
+                .value()
+                .join("") +
+              last
+            );
+          })
+          .value()
+      );
+
+      return (
+        first +
+        _(w)
+          .zip(s)
+          .flatten()
+          .join("")
+          .replace(/¤/g, "\n")
+      ).trim();
+    })
+    .value();
+}
